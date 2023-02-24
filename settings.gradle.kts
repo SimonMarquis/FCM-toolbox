f190c7f09fae052f230192e77878287475660c3d
Simon Marquis <contact@simon-marquis.fr>
2023-02-24 21:08:00 +0100

    ♻️ Big cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,21 @@
-plugins {
-    // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.51.0"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// https://docs.gradle.org/current/userguide/configuration_cache.html#config_cache:stable
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include(":app")
